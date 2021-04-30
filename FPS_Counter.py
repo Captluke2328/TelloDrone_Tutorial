e2fc1b309421c4fc26ecc956a6a2ea48ed750210
Captluke2328 <lukasjohnny2328@gmail.com>
2021-04-30 18:29:28 +0800

    +remove space

--- FPS_Counter.py
+++ FPS_Counter.py
@@ -7,6 +7,5 @@ while True:
 
     cv2.putText(img, str(fps), (40,100), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0),3)
     cv2.imshow("Result",img)
-
     if cv2.waitKey(1) & 0XFF == ord('q'):
         break
